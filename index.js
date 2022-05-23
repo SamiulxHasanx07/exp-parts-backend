169967591abde0d15ee5af5383bb291bb163bf0a
Samiul Hasan <samiulxhasan650@gmail.com>
2022-05-23 06:13:42 +0600

    Get Product by id API Done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -33,15 +33,20 @@ async function run() {
             const result = await productsCollection.insertOne(product);
             res.send(result)
         })
-
+        
         // Get All Products API
         app.get('/products', async (req, res) => {
             const result = await productsCollection.find({}).toArray();
             res.send(result)
         })
-
-
-
+        
+        //get signle product by id
+        app.get('/product/:id', async(req, res)=>{
+            const id =  req.params.id;
+            const query =  {_id:ObjectId(id)};
+            const result =  await productsCollection.findOne(query);
+            res.send(result);  
+        })
 
     }
     finally {
