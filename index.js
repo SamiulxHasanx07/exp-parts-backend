027cf0f9cc9fc08801642d4f06f7097a47630396
Samiul Hasan <samiulxhasan650@gmail.com>
2022-05-27 11:55:39 +0600

    error fix

--- index.js
+++ index.js
@@ -7,15 +7,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(
-    cors({
-        origin: true,
-        optionsSuccessStatus: 200,
-        credentials: true,
-    })
-);
-
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 // verify user token
